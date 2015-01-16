41a2bef43280cdec42699fef9b035258594f02ec
Justin Veirs <justinveirs@gmail.com>
2015-01-16 16:14:40 -0500

    bad shit is happening, fuck

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Cheeze", email: "cheeze@gmail.com")
+    @user = User.new(name: "Cheeze", email: "cheeze@gmail.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
