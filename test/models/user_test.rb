73a2b999bd3c6cf0d6c2e3e73e582415fabef83d
Justin Veirs <justinveirs@gmail.com>
2015-01-20 12:09:23 -0500

    syntax error

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -34,7 +34,7 @@ class UserTest < ActiveSupport::TestCase
     invalid_addresses = %w[user@example,com user@foo,com user_at_foo.org user.name@example.foo@bar_baz. foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
-      assert_not @user.valid? "#{invalid_address.inspect} should be invalid"
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 
@@ -49,5 +49,5 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  
+
 end
